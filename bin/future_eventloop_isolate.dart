e8cfc29f642f1498b5fa60b490ffe26ffaa99f46
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-08-03 20:43:33 +0300

    Isolate spawned, Handshake done

--- bin/future_eventloop_isolate.dart
+++ bin/future_eventloop_isolate.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
+  final stopwatch = Stopwatch()..start();
+  print('st $stopwatch');
+
   /*
   Исполняются сразу, результат завернут во Future
   */
@@ -28,7 +31,62 @@ void main(List<String> arguments) {
   Future(() => print('ordinary Future #2'))
       .whenComplete(() => print('whenComplete'));
 
+  print((stopwatch..stop()).elapsedMilliseconds);
+
+  print('Число потоков процессора: ${Platform.numberOfProcessors}');
+
+  final isolate = await initIsolate();
+  isolate.send('Hello world');
+
   //Завершает функцию(убивает текущий изолят)
-  // Isolate.current.kill();
   // exit(2);
+}
+
+Future<SendPort> initIsolate() async {
+  final completer = Completer<SendPort>();
+
+  // Создаём ReceivePort и начинаем его слушать
+  final isolateToMainStream = ReceivePort();
+  late StreamSubscription sub;
+  sub = isolateToMainStream.listen((data) {
+    if (data is SendPort) {
+      final mainToIsolateStream = data;
+      completer.complete(mainToIsolateStream);
+    } else {
+      print('[Main Isolate] $data');
+      //Отписываемся от ReceivePort и закрываем его
+      //чтобы программа завершилась, т.к. в ЭвентЛупе не будет заданий в очередях
+      //Т.е. помимо убийства изолята надо еще отписываться от него
+      sub.cancel();
+      isolateToMainStream.close();
+    }
+  });
+
+  // Спавним изолят, передаем туда top-level функцию(которую будем выполнять в изоляте)
+  // и передаем как аргумент/message SendPort (геттер из ReceivePort)
+  final createIsolateInstance =
+      await Isolate.spawn(createdIsolate, isolateToMainStream.sendPort);
+  final sendPortFromIsolate = await completer.future;
+  return sendPortFromIsolate;
+}
+
+void createdIsolate(SendPort isolateToMainStream) {
+  // Это отдельный Event Loop
+  // В этом изоляте создаём ReceivePort
+  final mainToIsolateStream = ReceivePort();
+
+  // Отправляем в исходный изолят SendPort этого изолята
+  isolateToMainStream.send(mainToIsolateStream.sendPort);
+
+  // Слушаем ReceivePort этого изолята, делаем принт,
+  // отправляем это сообщение обратно
+  mainToIsolateStream.listen((data) {
+    print('[Created Isolate] $data');
+    isolateToMainStream.send(data);
+
+    // После первого полученного сообщения убиваем изолят
+    // Если этого не сделать, то программа не завершится т.к.
+    // оба изолята слушаю друг друга, нужна отписка
+    Isolate.current.kill();
+  });
 }
